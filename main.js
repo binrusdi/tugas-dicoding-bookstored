e253e0d07599cbbf808283fdb5be4c2896ba9991
binrusdi <mrusdiana07@gmail.com>
2023-09-25 16:28:28 +0700

    update script

---
+++ main.js
@@ -0,0 +1,32 @@
+function saveData(event) {
+    event.preventDefault();
+    let judul = document.getElementById('judul').value;
+    let penulis = document.getElementById('penulis').value;
+    let tahun = document.getElementById('tahun').value;
+    let complete = document.getElementById('complete').checked;
+    console.log(complete);
+    let timestamp = +new Date();
+    let existingData = localStorage.getItem('data');
+    if (existingData) {
+        let parseData = JSON.parse(existingData);
+        if (parseData.judul === judul) {
+            alert('Nama sudah ada dilocal storage, silahkan pilih nama lain');
+            return;
+        }
+    }
+
+    let data = {
+        timestamp: timestamp,
+        judul: judul,
+        penulis: penulis,
+        tahun: tahun,
+        complete: complete
+    };
+
+    let jsonData = JSON.stringify(data);
+    localStorage.setItem('data', jsonData);
+    alert('Data berhasil disimpan')
+}
+
+let submit = document.querySelector('input[type="submit"]');
+submit.addEventListener('click', saveData);
