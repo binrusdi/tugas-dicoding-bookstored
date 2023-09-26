b77b6e2651120352148b9d53d111baea1c577b71
binrusdi <mrusdiana07@gmail.com>
2023-09-26 17:35:59 +0700

    front end

--- main.js
+++ main.js
@@ -1,34 +1,56 @@
-
-function saveData(event) {
-    event.preventDefault();
-    let judul = document.getElementById('judul').value;
-    let penulis = document.getElementById('penulis').value;
-    let tahun = document.getElementById('tahun').value;
-    let complete = document.getElementById('complete').checked;
-    console.log(complete);
-    let timestamp = +new Date();
-    let existingData = localStorage.getItem('data');
-    if (existingData) {
-        let parseData = JSON.parse(existingData);
-        if (parseData.judul === judul) {
-            alert('Nama sudah ada dilocal storage, silahkan pilih nama lain');
-            return;
-        }
-    }
-
-    let data = {
-        id: timestamp,
-        title: judul,
-        author: penulis,
-        year: tahun,
-        isComplete: complete
-    };
-
-    let jsonData = JSON.stringify(data);
-    localStorage.setItem('data', jsonData);
-    alert('Data berhasil disimpan')
+// Fungsi untuk mendapatkan data buku dari localStorage
+function getBookShelf() {
+    const bookShelfData = localStorage.getItem('bookShelf');
+    return bookShelfData ? JSON.parse(bookShelfData) : { unread: [], read: []}
 }
 
-let submit = document.querySelector('input[type="submit"]');
-console.log(submit);
-submit.addEventListener('click', saveData);
+// Fungsi untuk menyimpan data buku ke localStorage
+function saveBookShelf(bookShelf) {
+    localStorage.setItem('bookShelf', JSON.stringify(bookShelf));
+}
+
+// Fungsi untuk menambahkan buku baru
+function addBook(title, author, year, isComplete) {
+    const bookShelf = getBookShelf();
+
+    const newBook = {
+        id: +new Date(),
+        title,
+        author,
+        year,
+        isComplete
+    };
+
+    if (isComplete) {
+        bookShelf.read.push(newBook);
+    } else {
+        bookShelf.unread.push(newBook);
+    }
+
+    saveBookShelf(bookShelf);
+}
+
+// Fungsi untuk menghapus buku
+function removeBook(id) {
+    const bookShelf = getBookShelf();
+    bookShelf.unread = bookShelf.unread.filter(book => book.id !== id);
+    bookShelf.unread = bookShelf.read.filter(book => book.id !== id);
+    
+    saveBookShelf(bookShelf);
+}
+
+// Panggil fungsi addBook() ketika form disubmit
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const title = document.getElementById('judul').value;
+    const author = document.getElementById('penulis').value;
+    const year = parseInt(document.getElementById('tahun').value, 10);
+    const isComplete = document.getElementById('complete').checked;
+
+    addBook(title, author, year, isComplete);
+
+    window.location.reload();
+});
+
+// Menampilkan data read dan unread
+
